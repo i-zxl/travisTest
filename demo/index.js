35cc1ef3802d68643dcaf0a04cd4f0865a23c157
zxl <zxl-jacob@outlook.com>
2018-03-28 15:11:43 +0800

    。

--- demo/index.js
+++ demo/index.js
@@ -3,7 +3,7 @@ var start = require('../lib/index');
 var args = [].slice.call(process.argv, 2);
 
 if (args.length === 0) {
-  console.log('missing arguments: yd-cli ${projectName}');
+  console.log('missing arguments: zxl-cli ${projectName}');
   return process.exit();
 }
 var projectName = args[0];
